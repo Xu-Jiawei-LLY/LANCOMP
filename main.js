edb3b052c366a14eb87d14192d7b1823ea390f38
L033717 <xu_jiawei@lilly.com>
2026-04-14 16:25:27 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,26 @@
+const { app, BrowserWindow } = require('electron')
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1200,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: false,
+      contextIsolation: true
+    }
+  })
+
+  win.loadFile('index.html')  // points directly to your existing index.html
+}
+
+app.whenReady().then(() => {
+  createWindow()
+
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow()
+  })
+})
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit()
+})
